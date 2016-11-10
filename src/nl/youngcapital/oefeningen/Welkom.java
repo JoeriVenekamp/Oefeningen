4edcee303f0a758e1c3fc3b37898a2a392a35b5c
Joeri Venekamp <jvenekamp@gmail.com>
2016-11-10 10:36:39 +0100

    Welkom servlet

---
+++ src/nl/youngcapital/oefeningen/Welkom.java
@@ -0,0 +1,35 @@
+package nl.youngcapital.oefeningen;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Welkom
+ */
+@WebServlet("/Welkom")
+public class Welkom extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.getWriter()
+		
+		.append("<!DOCTYPE html><html><head>" +
+			"<title>mijn eerste servlet</title>" +
+			"<head><body><h1>Welkom</h1><br>" +
+			"</body></html>");
+			
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
